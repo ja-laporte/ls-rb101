70df62e76c1806b27355d7f36b22a3383fe4819a
Juan Laporte <ja_laporte@hotmail.com>
2021-04-11 22:10:41 -0400

    Updating lesson 3

---
+++ lesson_3/practice_problem_easy_1.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# What would you expect the code below to print out?
+
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
+
+<<~MYANSWER
+  Because uniq without the bang(!) is not destructive, puts numbers displays the original array [1,2,2,3].
+MYANSWER
+
+<<~SOLUTION
+  The text explains: numbers.uniq returned a new Array object with unique elements, but it did not modify the numbers object. Further, the puts method automatically calls to_s on its argument, and that’s why you see the output.
+  
+  Additional note: had the last line been p numbers instead, the output would have been [1, 2, 2, 3] because the p method automatically calls inspect on its argument, which gives a different formatting on the output. Furthermore, we could have also done puts numbers.inspect and the output would have been the same as using the p method.
+SOLUTION
