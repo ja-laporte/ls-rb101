53e535e982e07fe55be39ed59c5a72ab96dc5b7d
Juan Laporte <ja_laporte@hotmail.com>
2021-04-14 03:21:01 -0400

    Practice Problem 2-8

---
+++ lesson_3/practice_problem_easy_2_8.rb
@@ -0,0 +1,8 @@
+# Shorten this sentence:
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+# ...remove everything starting from "house".
+
+p advice.slice!(0..38)
+p advice
