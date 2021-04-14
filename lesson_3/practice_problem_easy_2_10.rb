752b96f614eb5ea7a987c40c66f01ca92f8c03c4
Juan Laporte <ja_laporte@hotmail.com>
2021-04-14 03:42:29 -0400

    Practice Problems 9 & 10

---
+++ lesson_3/practice_problem_easy_2_10.rb
@@ -0,0 +1,4 @@
+# If we had a 40 character wide table of Flintstone family members, how could we easily center that title above the table with spaces?
+
+title = "Flintstone Family Members"
+title.center(40)
