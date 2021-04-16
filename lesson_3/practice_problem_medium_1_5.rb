380ae6524246780e6b2fdaf17370e1f634ccf20f
Juan Laporte <ja_laporte@hotmail.com>
2021-04-16 00:42:00 -0400

    Unfinished problem, that needs to be worked on

---
+++ lesson_3/practice_problem_medium_1_5.rb
@@ -0,0 +1,17 @@
+# Ben coded up this implementation but complained that as soon as he ran it, he got an error. Something about the limit variable. What's wrong with the code?
+
+limit = 15
+
+def fib(first_num, second_num)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1)
+puts "result is #{result}"
+
+# How would you fix this so that it works?
