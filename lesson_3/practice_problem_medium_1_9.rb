3843c8164eb171a5ff2c2985de41557a69b74ca6
Juan Laporte <ja_laporte@hotmail.com>
2021-04-20 02:04:25 -0400

    medium 1 complete

---
+++ lesson_3/practice_problem_medium_1_9.rb
@@ -0,0 +1,16 @@
+# Consider these two simple methods:
+
+def foo(param = "no")
+  "yes"
+end
+
+def bar(param = "no")
+  param == "no" ? "yes" : "no"
+end
+
+# What would be the return value of the following method invocation?
+
+p bar(foo)
+# My answer:
+# This would return "yes" <- Incorrect
+# The foo method returns "yes", so when passed into the bar method, the paramater is changed to the return value of "yes" resulting in the ternary operator evaluating to false, which in turn returns "no".
