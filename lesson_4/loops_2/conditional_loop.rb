ce5f13fefd8453e2e8d3f9d704eb1f8c76d4f532
Juan Laporte <ja_laporte@hotmail.com>
2021-05-04 22:18:54 -0400

    loops 2 practice

---
+++ lesson_4/loops_2/conditional_loop.rb
@@ -0,0 +1,8 @@
+# Using an if/else statement, run a loop that prints "The loop was processed!" one time if process_the_loop equals true. Print "The loop wasn't processed!" if process_the_loop equals false.
+
+process_the_loop = [true, false].sample
+
+loop do
+  process_the_loop ? puts("The loop was processed!") : puts("The loop wasn't processed!")
+  break
+end
---
+++ lesson_4/loops_2/only_even.rb
@@ -0,0 +1,15 @@
+# Using next, modify the code below so that it only prints even numbers.
+
+number = 0
+# ---Original Code---
+# until number == 10
+#   number += 1
+#   puts number
+# end
+
+# ---Not My Solution---
+until number > 10
+  number += 1
+  next if number.odd?
+  puts number
+end
