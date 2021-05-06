4e2728d11f636c8999356f92ed4f2fb3321994b9
Juan Laporte <ja_laporte@hotmail.com>
2021-05-05 22:06:56 -0400

    methods pratice

--- lesson_4/methods_and_more_methods/practice_problem_1.rb
+++ lesson_4/methods_and_more_methods/practice_problem_1.rb
@@ -8,3 +8,8 @@ end
 # ---My Solution---
 # The select method will return a new collection given the truthiness of the block,
 # in this case, all elements are now 'hi': => ['hi', 'hi', 'hi']
+
+# ---Correct Solution---
+# The select method will return a new collection given the truthiness of the block.
+# Because it will always return 'hi', and it is a truthy value, it will return a new array
+# containing all the elements from the original array.
