15f95be84754603347462624bb656d81cf2cae04
Juan Laporte <ja_laporte@hotmail.com>
2021-05-30 20:44:23 -0400

    practice

--- lesson_5/practice_problem_13.rb
+++ lesson_5/practice_problem_13.rb
@@ -4,4 +4,11 @@ arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
 
 # The sorted array should look like this:
 
-[[1, 8, 3], [1, 6, 7], [1, 4, 9]]
+# [[1, 8, 3], [1, 6, 7], [1, 4, 9]]
+
+# ---Solution---
+arr.sort_by do |sub_arr|
+  sub_arr.select do |num|
+    num.odd?
+  end
+end
